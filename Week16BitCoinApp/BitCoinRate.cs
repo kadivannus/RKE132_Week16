70891f4e7d97329bc45a7ffb888b7c83364e32cf
Kadi01~ <kadi.vannus@tktk.ee>
2023-04-08 19:22:18 +0300

    add week 16 code

---
+++ Week16BitCoinApp/BitCoinRate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week16BitCoinApp
+{
+    public class BitCoinRate
+    {
+        public bpi bpi { get; set; }  
+    }
+
+    public class bpi
+    { 
+        public EUR EUR { get; set; }  
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+}
